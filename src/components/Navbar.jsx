6046acf1a75d72646e218a55708b6bca275e821a
Utsav Karan <ukaran11@gmail.com>
2021-10-09 19:40:24 +0530

    Utsav | Initial Skeleton

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd';
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
