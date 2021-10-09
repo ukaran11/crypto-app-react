6a49d883b0e481a86df9be62661d2c13c271666d
Utsav Karan <ukaran11@gmail.com>
2021-10-10 05:05:13 +0530

    Utsav | Added footer

--- src/App.js
+++ src/App.js
@@ -33,9 +33,18 @@ const App = () => {
                         </Switch>
                     </div>
                 </Layout>
-            </div>
-            <div className="footer">
+                <div className="footer" >
+                    <Typography.Title level={5} style={{ color: 'white', textAlign:'center'}}>
+                        Cryptoverse <br />
+                        All rights reserved
+                    </Typography.Title>
 
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
             </div>
         </div>
     )
