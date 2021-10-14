f9aad34ff1483e0a8056c512ba28e7688a411298
Utsav Karan <ukaran11@gmail.com>
2021-10-14 23:53:07 +0530

    Utsav | Started working on CryptoDetails

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,22 +1,28 @@
-import React, { useState} from 'react'
-import HTMLReactParser from 'html-react-parser';
-import { useParams } from 'react-router';
-import millify from 'millify';
-import { Col, Row, Typography, Select } from 'antd';
-import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+import React, { useState } from "react";
+import HTMLReactParser from "html-react-parser";
+import { useParams } from "react-router";
+import millify from "millify";
+import { Col, Row, Typography, Select } from "antd";
+import {
+  MoneyCollectOutlined,
+  DollarCircleOutlined,
+  FundOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+  CheckOutlined,
+  NumberOutlined,
+  ThunderboltOutlined,
+} from "@ant-design/icons";
 
 const { Title, Text } = Typography;
-const { Option }  = Select;
+const { Option } = Select;
 
 const CryptoDetails = () => {
-    const { coinId } = useParams();
-    const [timePeriod, setTimePeriod] = useState('7d');
+  const { coinId } = useParams();
+  const [timePeriod, setTimePeriod] = useState("7d");
 
-    return (
-        <div>
-            CryptoDetails { coinId}
-        </div>
-    )
-}
+  return <div>CryptoDetails {coinId}</div>;
+};
 
-export default CryptoDetails
+export default CryptoDetails;
