e158b66b183937b0023a71f338a8a4422696e4ec
Utsav Karan <ukaran11@gmail.com>
2021-10-14 18:41:47 +0530

    Utsav | Added CryptoDetails

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState} from 'react'
+import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router';
+import millify from 'millify';
+import { Col, Row, Typography, Select } from 'antd';
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+
+const { Title, Text } = Typography;
+const { Option }  = Select;
 
 const CryptoDetails = () => {
+    const { coinId } = useParams();
+    const [timePeriod, setTimePeriod] = useState('7d');
+
     return (
         <div>
-            CryptoDetails
+            CryptoDetails { coinId}
         </div>
     )
 }
